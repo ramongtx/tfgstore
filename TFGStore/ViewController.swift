31d28debce5d7d0e0bb1fd28bd3d64e63d1fd1e8
Ramon Carvalho Maciel <ramongtx@gmail.com>
2015-09-17 23:15:14 -0300

    Initial Commit

---
+++ TFGStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TFGStore
+//
+//  Created by Ramon Carvalho Maciel on 9/17/15.
+//  Copyright (c) 2015 Ramon Carvalho Maciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
